9257c06d7944828c73cf6fee4fe371cf13d6816b
smw0807 <smw0807@gmail.com>
2024-11-20 20:41:43 +0900

    코딩애플 리액트 4강

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,14 +1,33 @@
 import './App.css';
-
+import { useState } from 'react';
 function App() {
-  let post = '강남 고기 맛집';
+  // const [logo, setLogo] = useState('ReactBlog');
+  const logo = 'ReactBlog';
+  // 상태 변수 선언
+  // 0: 변수명, 1: 변수 값 설정하는 함수
+  const [title, setTitle] = useState([
+    '남자 코트 추천',
+    '강남 우동 맛집',
+    '파이썬 공부',
+  ]);
+
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={{ color: 'red', fontSize: '20px' }}>블로그임</h4>
+        <h4 style={{ color: 'red', fontSize: '20px' }}>{logo}</h4>
       </div>
-      <h4>블로그 글 제목</h4>
-      <div> {post} </div>
+      <div className="list">
+        <h4>{title[0]}</h4>
+        <p>2024년 11월 20일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>2024년 11월 20일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[2]}</h4>
+        <p>2024년 11월 20일 발행</p>
+      </div>
     </div>
   );
 }
