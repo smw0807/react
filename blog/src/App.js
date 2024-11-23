e057111db5cea7aef868ff1cc0c1ffc5e271a3fc
smw0807 <smw0807@gmail.com>
2024-11-23 15:15:39 +0900

    코딩애플 리액트 6강

--- blog/src/App.js
+++ blog/src/App.js
@@ -83,8 +83,19 @@ function App() {
         </h4>
         <p>2024년 11월 20일 발행</p>
       </div>
+
+      <Modal />
     </div>
   );
 }
 
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  );
+}
 export default App;
