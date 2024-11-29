54aba0dec0b2c80913276c78a2fd14cb3bc8113a
smw0807 <smw0807@gmail.com>
2024-11-29 20:35:28 +0900

    코딩애플 리액트 파트2 2강

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,19 +1,52 @@
 import './App.css';
 
-import { Button, Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
 function App() {
   return (
     <div>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">Shoe Shop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#cart">Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+
+      <div className="main-bg"></div>
+
+      <Container>
+        <Row>
+          <Col md={4}>
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width="80%"
+              alt="상품1"
+            />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </Col>
+          <Col md={4}>
+            <img
+              src="https://codingapple1.github.io/shop/shoes2.jpg"
+              width="80%"
+              alt="상품2"
+            />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </Col>
+          <Col md={4}>
+            <img
+              src="https://codingapple1.github.io/shop/shoes3.jpg"
+              width="80%"
+              alt="상품3"
+            />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
