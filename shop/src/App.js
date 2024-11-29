1a3eeb09994ff8d4cefd4adcdb694455157ed928
smw0807 <smw0807@gmail.com>
2024-11-29 20:12:17 +0900

    코딩애플 리액트 파트2 1강

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 
-import { Button } from 'react-bootstrap';
+import { Button, Navbar, Container, Nav } from 'react-bootstrap';
 function App() {
   return (
-    <div className="App">
-      <Button variant="primary">Primary</Button>
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
