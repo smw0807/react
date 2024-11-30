086124a95bc1aaaaeb1628b00d0e703425b94d0b
smw0807 <smw0807@gmail.com>
2024-11-30 17:24:31 +0900

    코딩애플 리액트 파트2 4강

--- shop/src/App.js
+++ shop/src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 
 import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
 import { data } from './data.js';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   const [shoes] = useState(data);
@@ -12,23 +13,18 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">Shoe Shop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="/" to="/">
+              Home
+            </Nav.Link>
             <Nav.Link href="#cart">Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <div className="main-bg"></div>
-
-      <Container>
-        <Row>
-          {shoes.map((v) => (
-            <Col md={4} key={v.id}>
-              <ColData shoes={v} />
-            </Col>
-          ))}
-        </Row>
-      </Container>
+      <Routes>
+        <Route path="/" element={<List shoes={shoes} />} />
+        <Route path="/detail" element={<Detail />} />
+      </Routes>
     </div>
   );
 }
@@ -40,6 +36,45 @@ function ColData(props) {
       <h4>{props.shoes.title}</h4>
       <p>{props.shoes.price}</p>
     </>
+  );
+}
+
+function List(props) {
+  return (
+    <>
+      <div className="main-bg"></div>
+      <Container>
+        <Row>
+          {props.shoes.map((v) => (
+            <Col md={4} key={v.id}>
+              <ColData shoes={v} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+function Detail() {
+  return (
+    <Container>
+      <Row>
+        <Col md={6}>
+          <img
+            src="https://codingapple1.github.io/shop/shoes1.jpg"
+            width="100%"
+            alt="상품"
+          />
+        </Col>
+        <Col md={6}>
+          <h4 className="pt-5">상품명</h4>
+          <p>상품설명</p>
+          <p>120000원</p>
+          <button className="btn btn-danger">주문하기</button>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 export default App;
