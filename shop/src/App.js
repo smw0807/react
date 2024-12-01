fb41843f824a5dd93f7f410e1db4f09318ebbb5b
smw0807 <smw0807@gmail.com>
2024-12-01 17:11:14 +0900

    코딩애플 리액트 파트2 6강

--- shop/src/App.js
+++ shop/src/App.js
@@ -3,20 +3,23 @@ import { useState } from 'react';
 
 import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
 import { data } from './data.js';
-import { Routes, Route, Link } from 'react-router-dom';
-
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import Detail from './routes/Detail.js';
 function App() {
   const [shoes] = useState(data);
+  /**
+   * useNavigate 함수는 페이지 이동을 할 수 있게 해주는 함수
+   */
+  const navigate = useNavigate();
   return (
-    <div>
+    <div className="App">
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">Shoe Shop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/" to="/">
-              Home
-            </Nav.Link>
-            <Nav.Link href="#cart">Cart</Nav.Link>
+            <Nav.Link onClick={() => navigate('/')}>Home</Nav.Link>
+            <Nav.Link onClick={() => navigate('/detail')}>Detail</Nav.Link>
+            <Nav.Link onClick={() => navigate('/about')}>About</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -24,6 +27,15 @@ function App() {
       <Routes>
         <Route path="/" element={<List shoes={shoes} />} />
         <Route path="/detail" element={<Detail />} />
+        <Route path="*" element={<div>404 Not Found</div>} />
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>멤버임</div>} />
+          <Route path="location" element={<div>위치임</div>} />
+        </Route>
+        <Route path="/event" element={<Event />}>
+          <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>} />
+          <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
+        </Route>
       </Routes>
     </div>
   );
@@ -56,25 +68,22 @@ function List(props) {
   );
 }
 
-function Detail() {
+function About() {
   return (
-    <Container>
-      <Row>
-        <Col md={6}>
-          <img
-            src="https://codingapple1.github.io/shop/shoes1.jpg"
-            width="100%"
-            alt="상품"
-          />
-        </Col>
-        <Col md={6}>
-          <h4 className="pt-5">상품명</h4>
-          <p>상품설명</p>
-          <p>120000원</p>
-          <button className="btn btn-danger">주문하기</button>
-        </Col>
-      </Row>
-    </Container>
+    <div>
+      <h4>회사소개임</h4>
+      <Outlet />
+    </div>
   );
 }
+
+function Event() {
+  return (
+    <div>
+      <h4>이벤트임</h4>
+      <Outlet />
+    </div>
+  );
+}
+
 export default App;
---
+++ shop/src/routes/Detail.js
@@ -0,0 +1,22 @@
+import { Container, Row, Col } from 'react-bootstrap';
+export default function Detail() {
+  return (
+    <Container>
+      <Row>
+        <Col md={6}>
+          <img
+            src="https://codingapple1.github.io/shop/shoes1.jpg"
+            width="100%"
+            alt="상품"
+          />
+        </Col>
+        <Col md={6}>
+          <h4 className="pt-5">상품명</h4>
+          <p>상품설명</p>
+          <p>120000원</p>
+          <button className="btn btn-danger">주문하기</button>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
