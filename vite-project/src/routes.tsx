7faaf08b0c923ceb6439e267f7f720864acfee66
smw0807 <smw0807@gmail.com>
2024-12-22 16:33:24 +0900

    파일 위치 수정

--- vite-project/src/routes.tsx
+++ vite-project/src/routes.tsx
@@ -1,17 +1,20 @@
+import IndexPage from './pages';
+import Signin from './pages/signin';
+
 export const routes = [
   {
     path: '/',
     // element: <GlobalLayout />,
-    // children: [
-    //   { path: "/", element: <Index /> },
-    //   { path: "/products", element: <PoroductsIndex /> },
-    //   { path: "/products/:id", element: <ProductsId /> },
-    // ],
+    children: [
+      { path: '/', element: <IndexPage /> },
+      { path: '/signin', element: <Signin /> },
+    ],
   },
 ];
 
 export const pages = [
   { route: '/' },
+  { route: '/signin' },
   // { route: "/products" },
   // { route: "/products/:id" },
 ];
