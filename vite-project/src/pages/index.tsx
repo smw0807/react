2b7a5545b1a8211eb03a671165ca41a2a5e8c698
smw0807 <smw0807@gmail.com>
2024-12-23 21:57:07 +0900

    로그아웃 테스트

--- vite-project/src/pages/index.tsx
+++ vite-project/src/pages/index.tsx
@@ -1,11 +1,19 @@
 import { hasToken } from '../common/Token';
 import { Navigate } from 'react-router-dom';
+import { Button } from '@mui/material';
+import { clearAllTokens } from '~/common/Token';
 const IndexPage = () => {
   // 토큰 없을 경우 로그인 페이지로
   if (!hasToken()) {
     return <Navigate to="/signin" />;
   }
-  return <div>IndexPage</div>;
+  return (
+    <div>
+      <Button variant="contained" onClick={clearAllTokens}>
+        로그아웃
+      </Button>
+    </div>
+  );
 };
 
 export default IndexPage;
