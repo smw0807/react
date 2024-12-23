1964c5e26f7fb6d1ce1e0f7385c9522cf3944124
smw0807 <smw0807@gmail.com>
2024-12-23 21:43:22 +0900

    프록시 설정 및 alias 설정

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import withReactRouter from 'vite-plugin-next-react-router';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,7 +13,17 @@ export default defineConfig({
       layout: '_layout',
     }),
   ],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+    },
+  },
   server: {
     port: 3000,
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_URL,
+      },
+    },
   },
 });
