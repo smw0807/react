ab9a6a12c0bd1e101232b0c6a536099dc31b1550
smw0807 <smw0807@gmail.com>
2024-12-23 13:19:36 +0900

    토큰 없을경우 로그인 페이지로 이동

--- vite-project/src/pages/index.tsx
+++ vite-project/src/pages/index.tsx
@@ -1,4 +1,10 @@
+import { hasToken } from '../common/Token';
+import { Navigate } from 'react-router-dom';
 const IndexPage = () => {
+  // 토큰 없을 경우 로그인 페이지로
+  if (!hasToken()) {
+    return <Navigate to="/signin" />;
+  }
   return <div>IndexPage</div>;
 };
 
