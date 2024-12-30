324f1be660fa7f374a08e2e695d566584256adb9
smw0807 <smw0807@gmail.com>
2024-12-30 16:01:35 +0900

    불필요 소스 제거

--- vite-firebase/src/components/UserAvatar.tsx
+++ vite-firebase/src/components/UserAvatar.tsx
@@ -7,25 +7,11 @@ type UserAvatarType = {
   photoURL: string;
 };
 export default function UserAvater(props: UserAvatarType) {
-  const [arrow] = useState<'Show' | 'Hide' | 'Center'>('Show');
   const [title] = useState(`${props.displayName}[${props.email}]`);
-  const mergedArrow = useMemo<TooltipProps['arrow']>(() => {
-    if (arrow === 'Hide') {
-      return false;
-    }
-
-    if (arrow === 'Show') {
-      return true;
-    }
-
-    return {
-      pointAtCenter: true,
-    };
-  }, [arrow]);
   return (
-    <Tooltip placement="bottom" title={title} arrow={mergedArrow}>
+    <Tooltip placement="bottom" title={title}>
       <Avatar size={50}>
-        <Image sizes="50px" src={props.photoURL} />
+        <Image src={props.photoURL} />
       </Avatar>
     </Tooltip>
   );
