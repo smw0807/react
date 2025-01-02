beb3ed908e29aaa5f323a6ba3261ef0249f5defc
smw0807 <smw0807@gmail.com>
2025-01-02 10:29:16 +0900

    글등록 기능 추가

--- vite-firebase/src/pages/Board.tsx
+++ vite-firebase/src/pages/Board.tsx
@@ -1,4 +1,4 @@
-import { Table } from 'antd';
+import { Row, Table } from 'antd';
 import Title from 'antd/es/typography/Title';
 import {
   collection,
@@ -7,10 +7,14 @@ import {
   getFirestore,
   onSnapshot,
   query,
+  addDoc,
 } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 import { useFirebaseApp } from '~/hooks/useFirebase';
+import BoardWrite from '~/components/BoardWrite';
+import { useAuth } from '~/hooks/useAuth';
 
+const COLLECTION_NAME = 'board';
 const columns = [
   {
     title: '제목',
@@ -44,7 +48,7 @@ export default function Board() {
   const firebaseApp = useFirebaseApp();
   const db = getFirestore(firebaseApp);
   useEffect(() => {
-    const q = query(collection(db, 'board'));
+    const q = query(collection(db, COLLECTION_NAME));
     const unsubscirbe = onSnapshot(q, (snapshot) => {
       const boardList = snapshot.docs.map((doc) => {
         return {
@@ -59,11 +63,29 @@ export default function Board() {
     };
   }, [db]);
 
+  // 글쓰기
+  const { user } = useAuth();
+  const handleWrite = async (data: DocumentData) => {
+    try {
+      await addDoc(collection(db, COLLECTION_NAME), {
+        ...data,
+        writerEmail: user?.email,
+        regDtime: new Date(),
+        updDtime: new Date(),
+      });
+    } catch (e) {
+      console.error('글쓰기 실패 : ', e);
+    }
+  };
+
   return (
     <div>
-      <Title level={2} style={{ margin: 0 }}>
-        게시판
-      </Title>
+      <Row justify="space-between" align="middle">
+        <Title level={2} style={{ margin: 0 }}>
+          게시판
+        </Title>
+        <BoardWrite handlWrite={handleWrite} />
+      </Row>
       <Table columns={columns} dataSource={board} rowKey="id" />
     </div>
   );
