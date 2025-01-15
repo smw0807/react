692e5b9d5a6b1796ae4e8842ee66ee04c8e3bb62
smw0807 <smw0807@gmail.com>
2025-01-15 11:29:02 +0900

    로그인 폼 수정

--- next-app/app/login/page.tsx
+++ next-app/app/login/page.tsx
@@ -4,25 +4,45 @@ import { Form, Input, Button, Card, Typography, Divider, Image } from 'antd';
 import { MailOutlined, LockOutlined } from '@ant-design/icons';
 const { Title } = Typography;
 
-interface Values {
-  username: string;
-  password: string;
-  remember: boolean;
-}
+// interface Values {
+//   email: string;
+//   password: string;
+// }
 
 export default function Login() {
-  const onFinish = (values: Values) => {
-    console.log('Received values of form: ', values);
-    if (values.remember) {
-      localStorage.setItem('username', values.username);
-      localStorage.setItem('password', values.password);
-    }
-  };
+  const [form] = Form.useForm();
+  const emailRules = [
+    { required: true, message: '이메일을 입력해주세요.' },
+    {
+      pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+      message: '이메일 형식이 올바르지 않습니다.',
+    },
+  ];
 
   const handleRegister = (e: FormEvent) => {
     e.preventDefault();
     console.log('Handle registration logic here');
   };
+
+  const handleLogin = async () => {
+    console.log('Handle login logic here');
+    const valid = await form.validateFields();
+    if (valid.errorFields && valid.errorFields.length > 0) {
+      console.log(valid.errorFields);
+      return;
+    }
+    const inputValues = form.getFieldsValue();
+    console.log(inputValues);
+  };
+
+  const handleGoogleLogin = () => {
+    console.log('Handle google login logic here');
+  };
+
+  const handleKakaoLogin = () => {
+    console.log('Handle kakao login logic here');
+  };
+
   return (
     <div
       style={{
@@ -36,16 +56,8 @@ export default function Login() {
         <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Title level={2}>UserAccess Hub </Title>
         </div>
-        <Form
-          name="normal_login"
-          className="login-form"
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-        >
-          <Form.Item
-            name="username"
-            rules={[{ required: true, message: 'Please input your E-mail!' }]}
-          >
+        <Form form={form} name="normal_login" className="login-form">
+          <Form.Item name="email" rules={emailRules}>
             <Input
               prefix={<MailOutlined className="site-form-item-icon" />}
               placeholder="E-mail"
@@ -53,7 +65,7 @@ export default function Login() {
           </Form.Item>
           <Form.Item
             name="password"
-            rules={[{ required: true, message: 'Please input your Password!' }]}
+            rules={[{ required: true, message: '패스워드를 입력해주세요.' }]}
           >
             <Input
               prefix={<LockOutlined className="site-form-item-icon" />}
@@ -65,9 +77,9 @@ export default function Login() {
           <Form.Item>
             <Button
               type="primary"
-              htmlType="submit"
               className="login-form-button"
               block
+              onClick={handleLogin}
             >
               로그인
             </Button>
@@ -78,11 +90,15 @@ export default function Login() {
           </Form.Item>
         </Form>
         <Divider>or</Divider>
-        <Button block style={{ marginBottom: '10px' }}>
+        <Button
+          block
+          style={{ marginBottom: '10px' }}
+          onClick={handleGoogleLogin}
+        >
           <Image src="/google.svg" alt="google" style={{ width: '20px' }} />
-          구글로그인
+          구글 로그인
         </Button>
-        <Button block>
+        <Button block onClick={handleKakaoLogin}>
           <Image src="/kakaotalk.svg" alt="kakao" style={{ width: '20px' }} />
           카카오 로그인
         </Button>
