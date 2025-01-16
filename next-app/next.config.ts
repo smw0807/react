3ecfce70c593c562e40d77563b8c7541cf442677
smw0807 <smw0807@gmail.com>
2025-01-16 11:05:47 +0900

    프록시 설정

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // 프록시 설정
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:5001/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
