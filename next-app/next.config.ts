eac8e7ac4c6468a0d74aa113a410b029e32e8853
smw0807 <smw0807@gmail.com>
2025-01-16 11:45:37 +0900

    프록시 설정 env값으로 수정

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5001/api/:path*',
+        destination: `${process.env.NEXT_API_URL}/api/:path*`,
       },
     ];
   },
