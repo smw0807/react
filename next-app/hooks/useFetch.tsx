f74c52359d877710b9a9d8b00b8a6daced624629
smw0807 <smw0807@gmail.com>
2025-01-16 11:16:40 +0900

    fetch hook 추가

---
+++ next-app/hooks/useFetch.tsx
@@ -0,0 +1,11 @@
+export const useFetch = () => {
+  return async (url: string, options: RequestInit) => {
+    const res = await fetch(url, {
+      ...options,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return res.json();
+  };
+};
