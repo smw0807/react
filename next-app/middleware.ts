669bae8ce739a8b47ab3cefd60e5769ee46176ac
smw0807 <smw0807@gmail.com>
2025-01-19 20:17:04 +0900

    토큰처리 관련 미들웨어 추가

---
+++ next-app/middleware.ts
@@ -0,0 +1,131 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { cookies } from 'next/headers';
+
+const fetchAPI = async (url: string, token: string) => {
+  try {
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `${token}`,
+      },
+    });
+    return await res.json();
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
+
+/**
+ * 토큰 정보 확인
+ * @param token
+ * @returns
+ */
+const userInfo = (token: string) =>
+  fetchAPI(`${process.env.NEXT_API_URL}/api/auth/verify/token`, token);
+
+/**
+ * 리프레시 토큰 이용해 토큰 갱신
+ * @param token
+ * @returns
+ */
+const getRefreshToken = (token: string) =>
+  fetchAPI(`${process.env.NEXT_API_URL}/api/auth/refresh/token`, token);
+
+/**
+ * 리프레시 토큰 이용해 토큰 갱신
+ * @param request
+ * @returns
+ */
+const refreshTokenProcess = async (request: NextRequest) => {
+  const refreshToken = request.cookies.get(
+    process.env.NEXT_PUBLIC_REFRESH_TOKEN_NAME!
+  );
+  if (refreshToken) {
+    const user = await userInfo(refreshToken.value);
+    const cookieStore = await cookies();
+    if (user.success) {
+      const newAccessToken = await getRefreshToken(refreshToken.value);
+      // 새로운 토큰 쿠키에 저장
+      const response = NextResponse.next();
+      response.cookies.set(
+        process.env.NEXT_PUBLIC_ACCESS_TOKEN_NAME!,
+        newAccessToken.token.access_token
+      );
+      response.cookies.set(
+        process.env.NEXT_PUBLIC_REFRESH_TOKEN_NAME!,
+        newAccessToken.token.refresh_token
+      );
+      cookieStore.set(
+        process.env.NEXT_PUBLIC_ACCESS_TOKEN_NAME!,
+        newAccessToken.token.access_token
+      );
+      cookieStore.set(
+        process.env.NEXT_PUBLIC_REFRESH_TOKEN_NAME!,
+        newAccessToken.token.refresh_token
+      );
+      return response;
+    } else {
+      // 쿠키 제거
+      const response = NextResponse.redirect(new URL('/login', request.url));
+      response.cookies.delete(process.env.NEXT_PUBLIC_ACCESS_TOKEN_NAME!);
+      response.cookies.delete(process.env.NEXT_PUBLIC_REFRESH_TOKEN_NAME!);
+      cookieStore.delete(process.env.NEXT_PUBLIC_ACCESS_TOKEN_NAME!);
+      cookieStore.delete(process.env.NEXT_PUBLIC_REFRESH_TOKEN_NAME!);
+      return response;
+    }
+  }
+};
+
+export async function middleware(request: NextRequest) {
+  // 루트 페이지 접근 시
+  if (request.nextUrl.pathname === '/') {
+    // 쿠키에 저장된 토큰 확인
+    const accessToken = request.cookies.get(
+      process.env.NEXT_PUBLIC_ACCESS_TOKEN_NAME!
+    );
+    // 토큰 정보가 없으면 login 페이지로 이동
+    if (!accessToken) {
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+    // 토큰 정보가 있으면 토큰 정보를 이용해 사용자 정보 가져오기
+    const user = await userInfo(accessToken.value);
+    if (user.success) {
+      const role = user.data.role;
+      if (role === 'ADMIN') {
+        return NextResponse.redirect(new URL('/manage/users', request.url));
+      } else if (role === 'USER') {
+        return NextResponse.redirect(new URL('/user', request.url));
+      }
+    }
+    // 어세스 토큰 만료 시 리프레시 토큰 이용해 토큰 갱신
+    return await refreshTokenProcess(request);
+  }
+
+  // 관리자 또는 사용자 페이지 접근 시
+  const isAdmin = request.nextUrl.pathname.startsWith('/manage');
+  const isUser = request.nextUrl.pathname.startsWith('/user');
+  if (isAdmin || isUser) {
+    const accessToken = request.cookies.get(
+      process.env.NEXT_PUBLIC_ACCESS_TOKEN_NAME!
+    );
+    // 토큰 정보가 없으면 login 페이지로 이동
+    if (!accessToken) {
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+    const user = await userInfo(accessToken.value);
+    if (user.success) {
+      return NextResponse.next();
+    }
+    // 어세스 토큰 만료 시 리프레시 토큰 이용해 토큰 갱신
+    return await refreshTokenProcess(request);
+  }
+  return NextResponse.next();
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/:path*',
+};
