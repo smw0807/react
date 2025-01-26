b7a626b133e0b3df34314d688c758c4efa88203a
smw0807 <smw0807@gmail.com>
2025-01-26 23:26:46 +0900

    로그아웃 추가

--- next-app/app/manage/layout.tsx
+++ next-app/app/manage/layout.tsx
@@ -1,9 +1,11 @@
 'use client';
 import { useState } from 'react';
-import { Layout, theme } from 'antd';
+import { Layout, Modal, theme } from 'antd';
 import { NavigationComponent } from '~/components/Navigation';
 import { HeaderComponent } from '~/components/Header';
 import { Footer } from 'antd/es/layout/layout';
+import { useToken } from '~/common/useToken';
+import { useRouter } from 'next/navigation';
 
 const { Content } = Layout;
 export default function ManageLayout({
@@ -11,10 +13,26 @@ export default function ManageLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const [collapsed, setCollapsed] = useState(false);
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
+  const [collapsed, setCollapsed] = useState(false);
+
+  const { removeToken } = useToken();
+  const router = useRouter();
+  const [api, modal] = Modal.useModal();
+  const handleLogout = () => {
+    api.confirm({
+      title: '정말 로그아웃 하시겠습니까?',
+      content: '로그아웃 하면 다시 로그인 해야합니다.',
+      okText: '로그아웃',
+      cancelText: '취소',
+      onOk: () => {
+        removeToken('all');
+        router.push('/login');
+      },
+    });
+  };
   return (
     <>
       <Layout style={{ minHeight: '100vh' }}>
@@ -23,7 +41,11 @@ export default function ManageLayout({
           setCollapsed={setCollapsed}
         />
         <Layout>
-          <HeaderComponent collapsed={collapsed} setCollapsed={setCollapsed} />
+          <HeaderComponent
+            collapsed={collapsed}
+            setCollapsed={setCollapsed}
+            handleLogout={handleLogout}
+          />
 
           <Content
             style={{
@@ -34,6 +56,7 @@ export default function ManageLayout({
               borderRadius: borderRadiusLG,
             }}
           >
+            {modal}
             {children}
           </Content>
           <Footer style={{ textAlign: 'center' }}>
--- next-app/components/Header.tsx
+++ next-app/components/Header.tsx
@@ -1,29 +1,40 @@
-import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
-import { Button, Layout, theme } from 'antd';
+import {
+  LogoutOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+} from '@ant-design/icons';
+import { Button, Layout, Row, theme } from 'antd';
 
 const { Header } = Layout;
 export const HeaderComponent = ({
   collapsed,
   setCollapsed,
+  handleLogout,
 }: {
   collapsed: boolean;
   setCollapsed: (collapsed: boolean) => void;
+  handleLogout: () => void;
 }) => {
   const {
     token: { colorBgContainer },
   } = theme.useToken();
   return (
     <Header style={{ padding: 0, background: colorBgContainer }}>
-      <Button
-        type="text"
-        icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-        onClick={() => setCollapsed(!collapsed)}
-        style={{
-          fontSize: '16px',
-          width: 64,
-          height: 64,
-        }}
-      />
+      <Row justify="space-between" align="middle">
+        <Button
+          type="text"
+          icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+          onClick={() => setCollapsed(!collapsed)}
+          style={{
+            fontSize: '16px',
+            width: 64,
+            height: 64,
+          }}
+        />
+        <Button type="text" icon={<LogoutOutlined />} onClick={handleLogout}>
+          로그아웃
+        </Button>
+      </Row>
     </Header>
   );
 };
