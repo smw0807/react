77188840e87f9fbe9e6d72ab160708d498ce47a4
smw0807 <smw0807@gmail.com>
2025-02-04 15:15:57 +0900

    사용자정보 표기

--- next-app/app/user/layout.tsx
+++ next-app/app/user/layout.tsx
@@ -1,7 +1,11 @@
+'use client';
+import useAuth from '~/hooks/useAuth';
+
 export default function UserLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  useAuth();
   return <div>{children}</div>;
 }
--- next-app/app/user/page.tsx
+++ next-app/app/user/page.tsx
@@ -1,8 +1,30 @@
+'use client';
+import { Descriptions } from 'antd';
+import { useEffect, useState } from 'react';
+import { useFetch } from '~/common/useFetch';
 import { LoadingComponent } from '~/components/Loading';
 export default function User() {
+  const [user, setUser] = useState();
+  const fetchData = useFetch();
+  useEffect(() => {
+    (async () => {
+      const res = await fetchData('/api/user/myInfo');
+      if (res.success) {
+        const keys = Object.keys(res.user);
+        const items = keys.map((key) => ({
+          key,
+          label: key,
+          children: `${res.user[key]}`,
+        }));
+        console.log(items);
+        setUser(items);
+      }
+    })();
+  }, []);
+
   return (
     <div>
-      <LoadingComponent content="유저 페이지 작업 대기중입니다." />
+      <Descriptions title="User Info" bordered items={user} />;
     </div>
   );
 }
