18b2caff719dec968333905eb127ed8cef71b198
smw0807 <smw0807@gmail.com>
2025-02-04 11:04:00 +0900

    사용자 장보 저장 훅

--- next-app/hooks/useAuth.ts
+++ next-app/hooks/useAuth.ts
@@ -1,15 +1,27 @@
+import { useCookies } from 'next-client-cookies';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '~/store';
 import { UserState, setUser } from '~/store/userSlice';
 
-export const useAuth = () => {
+const useAuth = () => {
   const user = useSelector((state: RootState) => state.userStore.user);
   const dispatch: AppDispatch = useDispatch();
 
   const dispatchUser = (user: UserState) => {
-    console.log(user);
     dispatch(setUser(user));
   };
 
+  const cookieStore = useCookies();
+  useEffect(() => {
+    if (!user) {
+      const userInfo = cookieStore.get(process.env.NEXT_PUBLIC_USER_INFO_NAME!);
+      if (userInfo) {
+        dispatchUser(JSON.parse(userInfo));
+      }
+    }
+  }, [user]);
   return { user, dispatchUser };
 };
+
+export default useAuth;
