0827fe786ab0ad029a8b44cc9d4cf4ee5c8da7cb
smw0807 <smw0807@gmail.com>
2025-04-09 21:25:15 +0900

    리액트 강의 - Calendar

--- part1_wedding/src/App.tsx
+++ part1_wedding/src/App.tsx
@@ -10,6 +10,7 @@ import Video from '@components/sections/Video'
 import ImageGallery from '@components/sections/ImageGallery'
 import Intro from '@components/sections/Intro'
 import Invitation from '@components/sections/Invitation'
+import Calendar from '@components/sections/Calendar'
 
 import { Wedding } from '@models/wedding'
 
@@ -73,6 +74,7 @@ function App() {
       />
       <Invitation message={invitation} />
       <ImageGallery images={galleryImages} />
+      <Calendar date={date} />
       {JSON.stringify(wedding)}
     </div>
   )
---
+++ part1_wedding/src/components/sections/Calendar.tsx
@@ -0,0 +1,54 @@
+import classNames from 'classnames/bind'
+import { parseISO, format } from 'date-fns'
+import { ko } from 'date-fns/locale'
+import { DayPicker } from 'react-day-picker'
+
+import Section from '@shared/Section'
+
+import style from './Calendar.module.scss'
+import 'react-day-picker/dist/style.css'
+
+const cx = classNames.bind(style)
+
+const css = `
+  .rdp-month_caption {
+    display: none;
+  }
+  .rdp-cell {
+    cursor: default;
+  }
+  .rdp-head_cell {
+    font-weight: 500;
+    font-size: 14px;
+  }
+`
+function Calendar({ date }: { date: string }) {
+  const weddingDate = parseISO(date)
+  return (
+    <Section
+      title={
+        <div className={cx('wrap-header')}>
+          <span className={cx('txt-date')}>
+            {format(weddingDate, 'yyyy.MM.dd')}
+          </span>
+          <span className={cx('txt-time')}>
+            {format(weddingDate, 'aaa h시 eeee', { locale: ko })}
+          </span>
+        </div>
+      }
+    >
+      <div className={cx('wrap-calendar')}>
+        <style>{css}</style>
+        <DayPicker
+          locale={ko}
+          month={weddingDate}
+          selected={weddingDate}
+          formatters={{ formatCaption: () => '' }}
+          hideNavigation
+        />
+      </div>
+    </Section>
+  )
+}
+
+export default Calendar
