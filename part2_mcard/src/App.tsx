83511ecd791a0a848fbb26797185a344c8b29429
smw0807 <smw0807@gmail.com>
2025-04-19 22:16:49 +0900

    리액트 강의 - input, alert, flex box

--- part2_mcard/src/App.tsx
+++ part2_mcard/src/App.tsx
@@ -3,10 +3,13 @@ import { css } from '@emotion/react'
 import styled from '@emotion/styled'
 import Text from '@shared/Text'
 import Button from '@shared/Button'
-import logo from './logo.svg'
+import Input from '@shared/Input'
+import TextField from '@shared/TextField'
+import Alert from '@shared/Alert'
 import './App.css'
-
+import { useAlertContext } from '@contexts/AlertContext'
 function App() {
+  const { open } = useAlertContext()
   return (
     <div>
       <Text typography="t1" display="block" color="red">
@@ -41,6 +44,39 @@ function App() {
       </Button>
       <Button color="success" size="large" full disabled>
         클릭해주세요
+      </Button>
+
+      <div style={{ height: 10, width: '100%', background: '#efefef' }}></div>
+      <Input placeholder="이름을 입력해주세요" />
+      <Input aria-invalid />
+      <div style={{ height: 10, width: '100%', background: '#efefef' }}></div>
+      <TextField label="이름" />
+      <TextField label="이름" hasError />
+      <TextField
+        label="이름"
+        hasError
+        helpMessage="영문 대소문자, 숫자만 입력해주세요"
+      />
+
+      <div style={{ height: 10, width: '100%', background: '#efefef' }}></div>
+      {/* <Alert
+        open
+        title="알림"
+        description="알림 내용"
+        buttonLabel="확인"
+        onButtonClick={() => {}}
+      /> */}
+      <Button
+        onClick={() =>
+          open({
+            title: '알림',
+            description: '알림 내용',
+            buttonLabel: '확인',
+            onButtonClick: () => {},
+          })
+        }
+      >
+        알림 열기
       </Button>
     </div>
   )
