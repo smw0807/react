c4f69cd36d89f98bae818e706116c3fcea06492f
smw0807 <smw0807@gmail.com>
2025-04-23 07:49:48 +0900

    리액트 강의 - 회원가입 구현

--- part2_mcard/src/pages/Signup.tsx
+++ part2_mcard/src/pages/Signup.tsx
@@ -1,8 +1,30 @@
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { collection, doc, setDoc } from 'firebase/firestore'
+import { auth, store } from '@remote/firebase'
+
 import SignupForm from '@components/signup/Form'
+import { FormValues } from '@models/signup'
+import { COLLECTIONS } from '@constants'
 function Signup() {
+  const handleSubmit = async (formValues: FormValues) => {
+    const { email, password } = formValues
+    const { user } = await createUserWithEmailAndPassword(auth, email, password)
+    await updateProfile(user, {
+      displayName: formValues.name,
+    })
+
+    const newUser = {
+      uid: user.uid,
+      email: user.email,
+      displayName: formValues.name,
+    }
+
+    await setDoc(doc(collection(store, COLLECTIONS.USER), user.uid), newUser)
+  }
+
   return (
     <div>
-      <SignupForm />
+      <SignupForm onSubmit={handleSubmit} />
     </div>
   )
 }
