e5a2aaa243a2e256b146ea2d78994dea7bd8fc04
smw0807 <smw0807@gmail.com>
2025-04-26 21:47:56 +0900

    리액트강의 - 카드신청구현>약관동의

--- part2_mcard/src/components/apply/Terms.tsx
+++ part2_mcard/src/components/apply/Terms.tsx
@@ -1,5 +1,71 @@
-function Terms() {
-  return <div>Terms</div>
+import { MouseEvent, useCallback, useState } from 'react'
+import FixedBottomButton from '@shared/FixedBottomButton'
+
+import Agreement from '@shared/Agreement'
+
+import { 약관목록 } from '@constants/apply'
+
+function Terms({ onNext }: { onNext: (terms: string[]) => void }) {
+  const [termsAgreements, setTermsAgreements] = useState(() => {
+    return 약관목록.reduce(
+      (prev, term) => ({
+        ...prev,
+        [term.id]: false,
+      }),
+      {},
+    )
+  })
+
+  const isAllAgreed = Object.values(termsAgreements).every(
+    (agreement) => agreement,
+  )
+
+  const handleAllAgreement = useCallback(
+    (_: MouseEvent<HTMLElement>, checked: boolean) => {
+      setTermsAgreements((prev) => {
+        return Object.keys(prev).reduce(
+          (acc, key) => ({
+            ...acc,
+            [key]: checked,
+          }),
+          {},
+        )
+      })
+    },
+    [],
+  )
+
+  return (
+    <div>
+      <Agreement>
+        <Agreement.Title checked={isAllAgreed} onChange={handleAllAgreement}>
+          약관에 모두 동의
+        </Agreement.Title>
+        {약관목록.map(({ id, title, link }) => (
+          <Agreement.Description
+            key={id}
+            link={link}
+            checked={termsAgreements[id as keyof typeof termsAgreements]}
+            onChange={(_, checked) => {
+              setTermsAgreements((prev) => ({
+                ...prev,
+                [id]: checked,
+              }))
+            }}
+          >
+            {title}
+          </Agreement.Description>
+        ))}
+      </Agreement>
+      <FixedBottomButton
+        label="약관동의"
+        disabled={!isAllAgreed}
+        onClick={() => {
+          onNext(Object.keys(termsAgreements))
+        }}
+      />
+    </div>
+  )
 }
 
 export default Terms
--- part2_mcard/src/pages/Apply.tsx
+++ part2_mcard/src/pages/Apply.tsx
@@ -7,9 +7,13 @@ import CardInfo from '@components/apply/CardInfo'
 function ApplyPage() {
   const [step, setStep] = useState(0)
 
+  const handleTermsChange = (terms: string[]) => {
+    console.log(terms)
+  }
+
   return (
     <div>
-      {step === 0 && <Terms />}
+      {step === 0 && <Terms onNext={handleTermsChange} />}
       {step === 1 && <BasicInfo />}
       {step === 2 && <CardInfo />}
     </div>
