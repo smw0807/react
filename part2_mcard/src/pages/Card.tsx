3bfd20f7066ef3d7b269231b5b17796dea77c8ae
smw0807 <smw0807@gmail.com>
2025-05-01 22:53:16 +0900

    리액트 강의 - 병목코드 제거

--- part2_mcard/src/pages/Card.tsx
+++ part2_mcard/src/pages/Card.tsx
@@ -108,20 +108,7 @@ function IconCheck() {
 }
 
 function removeHtmlTags(text: string) {
-  let output = ''
-  for (let i = 0; i < text.length; i++) {
-    if (text[i] === '<') {
-      for (let j = i + 1; j < text.length; j += 1) {
-        if (text[j] === '>') {
-          i = j
-          break
-        }
-      }
-    } else {
-      output += text[i]
-    }
-  }
-  return output
+  return text.replace(/<\/?[^>]+(>|$)/g, '')
 }
 
 const TermsContainer = styled(Flex)`
