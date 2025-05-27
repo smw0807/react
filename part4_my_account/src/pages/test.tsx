62240cab09935cb4f17ea5f1a9d200a8755f218c
smw0807 <smw0807@gmail.com>
2025-05-27 21:18:28 +0900

    리액트 강의 - 이벤트 배너 데이터 준비

---
+++ part4_my_account/src/pages/test.tsx
@@ -0,0 +1,11 @@
+import EventBannerAddButton from '@components/test/EventBannerAddButton'
+
+function Test() {
+  return (
+    <div>
+      <EventBannerAddButton />
+    </div>
+  )
+}
+
+export default Test
