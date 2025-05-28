8a00fe14f3a2bf47aa9a5d39129b2ed0c651f724
smw0807 <smw0807@gmail.com>
2025-05-28 23:47:08 +0900

    홈 - 신용점수

---
+++ part4_my_account/src/components/home/CreditScore.tsx
@@ -0,0 +1,43 @@
+import CreditScoreChart from '@shared/CreditScoreChart'
+
+import Flex from '@shared/Flex'
+import Text from '@shared/Text'
+import Button from '@shared/Button'
+import Spacing from '@shared/Spacing'
+import Skeleton from '@shared/Skeleton'
+
+function CreditScore() {
+  return (
+    <div style={{ padding: 24 }}>
+      <Flex justify="space-between" align="center">
+        <Flex direction="column">
+          <Text bold>
+            나의 신용도를 증명하고
+            <br />
+            점수를 올리세요
+          </Text>
+          <Spacing size={8} />
+          <Button>내 신용점수 보러가기</Button>
+        </Flex>
+        <CreditScoreChart score={500} width={80} height={80} />
+      </Flex>
+    </div>
+  )
+}
+
+export function CreditScoreSkeleton() {
+  return (
+    <div style={{ padding: 24 }}>
+      <Flex justify="space-between" align="center">
+        <Flex direction="column">
+          <Skeleton width={155} height={50} />
+
+          <Spacing size={8} />
+          <Skeleton width={155} height={31} />
+        </Flex>
+      </Flex>
+    </div>
+  )
+}
+
+export default CreditScore
--- part4_my_account/src/pages/index.tsx
+++ part4_my_account/src/pages/index.tsx
@@ -1,18 +1,26 @@
 import dynamic from 'next/dynamic'
-import Skeleton from '@shared/Skeleton'
+import Spacing from '@shared/Spacing'
 
 import Account from '@components/home/Account'
 import { EventBannersSkeleton } from '@components/home/EventBanners'
+import { CreditScoreSkeleton } from '@components/home/CreditScore'
 
 const EventBanners = dynamic(() => import('@components/home/EventBanners'), {
   ssr: false,
   loading: () => <EventBannersSkeleton />,
+})
+
+const CreditScore = dynamic(() => import('@components/home/CreditScore'), {
+  ssr: false,
+  loading: () => <CreditScoreSkeleton />,
 })
 export default function Home() {
   return (
     <>
       <EventBanners />
       <Account />
+      <Spacing size={8} backgroundColor="gray100" />
+      <CreditScore />
     </>
   )
 }
