2fe1283e943a35156a29802baa651ca54519496a
smw0807 <smw0807@gmail.com>
2025-06-15 21:28:05 +0900

    리액트 강의 - 월별 자산 그래프

--- part4_my_account/src/pages/account/index.tsx
+++ part4_my_account/src/pages/account/index.tsx
@@ -2,13 +2,26 @@ import withAuth from '@/hooks/withAuth'
 import dynamic from 'next/dynamic'
 
 const Transactions = dynamic(() => import('@/components/account/Transactions'))
+const MonthlyChart = dynamic(() => import('@/components/account/MonthlyChart'))
 
 function AccountPage() {
   return (
     <div>
+      <MonthlyChart chartData={generateMonthlyChartData()} />
       <Transactions />
     </div>
   )
+}
+
+function generateMonthlyChartData() {
+  const data = []
+  for (let i = 1; i <= 12; i++) {
+    data.push({
+      date: new Date(2025, i, 1).toISOString(),
+      balance: Math.floor(Math.random() * (1000000 - 100000) + 100000),
+    })
+  }
+  return data
 }
 
 export default withAuth(AccountPage)
