c33600effbff4d81f3bbbd6b278789095ac77e4f
smw0807 <smw0807@gmail.com>
2025-06-16 22:00:40 +0900

    리액트 강의 - 약관 목록

--- part4_my_account/src/pages/my.tsx
+++ part4_my_account/src/pages/my.tsx
@@ -1,17 +1,34 @@
 import { signOut } from 'next-auth/react'
+import { useRouter } from 'next/router'
 import withAuth from '@/hooks/withAuth'
 
 import Flex from '@shared/Flex'
 import Spacing from '@shared/Spacing'
 import Button from '@shared/Button'
+import ListRow from '@shared/ListRow'
 
 function MyPage() {
+  const navigate = useRouter()
   return (
     <div>
       <Spacing size={100} />
       <Flex justify="center">
         <Button onClick={() => signOut({ callbackUrl: '/' })}>로그아웃</Button>
       </Flex>
+
+      <Spacing
+        size={8}
+        backgroundColor="gray100"
+        style={{ margin: '20px 0' }}
+      />
+
+      <ul>
+        <ListRow
+          content={<ListRow.Texts title="약관" subtitle="약관목록 및 철회" />}
+          withArrow
+          onClick={() => navigate.push('/settings/terms')}
+        />
+      </ul>
     </div>
   )
 }
---
+++ part4_my_account/src/pages/settings/terms.tsx
@@ -0,0 +1,98 @@
+import { useMemo } from 'react'
+import { GetServerSidePropsContext } from 'next'
+import { getSession } from 'next-auth/react'
+import { dehydrate, QueryClient, useQuery } from 'react-query'
+
+import useUser from '@/hooks/useUser'
+import { getTerms } from '@remote/account'
+import { User } from '@/models/user'
+import { 약관목록 } from '@/constants/account'
+
+import Top from '@shared/Top'
+import Text from '@shared/Text'
+import ListRow from '@shared/ListRow'
+import Button from '@shared/Button'
+
+function TermsPage() {
+  const user = useUser()
+  const { data: terms } = useQuery(
+    ['terms', user?.id],
+    () => getTerms(user?.id as string),
+    {
+      enabled: user != null,
+    },
+  )
+  const 동의한약관목록 = useMemo(() => {
+    if (terms == null) {
+      return null
+    }
+
+    const 동의한전체약관목록 = 약관목록.filter((약관) =>
+      terms.termIds.includes(약관.id),
+    )
+
+    const 필수약관목록 = 동의한전체약관목록.filter((약관) => 약관.mandatory)
+    const 선택약관목록 = 동의한전체약관목록.filter((약관) => !약관.mandatory)
+
+    return {
+      필수약관목록,
+      선택약관목록,
+    }
+  }, [terms])
+  console.log('동의한약관목록 : ', 동의한약관목록)
+  return (
+    <div>
+      <Top title="약관" subtitle="약관목록 및 철회" />
+      {동의한약관목록 == null ? (
+        <Text>동의한 약관 목록이 없습니다.</Text>
+      ) : (
+        <ul>
+          {동의한약관목록.필수약관목록.map((term) => {
+            return (
+              <ListRow
+                key={term.id}
+                content={
+                  <ListRow.Texts title={`[필수] ${term.title}`} subtitle="" />
+                }
+              />
+            )
+          })}
+          {동의한약관목록.선택약관목록.map((term) => {
+            return (
+              <ListRow
+                key={term.id}
+                content={
+                  <ListRow.Texts title={`[선택] ${term.title}`} subtitle="" />
+                }
+                right={<Button>철회</Button>}
+              />
+            )
+          })}
+        </ul>
+      )}
+    </div>
+  )
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getSession(context)
+  if (session != null && session.user != null) {
+    const client = new QueryClient()
+
+    await client.prefetchQuery(['terms', (session.user as User).id], () =>
+      getTerms((session.user as User).id as string),
+    )
+
+    return {
+      props: {
+        dehydratedState: JSON.parse(JSON.stringify(dehydrate(client))),
+      },
+    }
+  }
+
+  return {
+    props: {},
+  }
+}
+
+export default TermsPage
