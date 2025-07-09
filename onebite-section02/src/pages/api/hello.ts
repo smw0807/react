dc71a852fa134224d4c3db9c441dab40b02931a5
smw0807 <smw0807@gmail.com>
2025-07-09 21:52:54 +0900

    API Routes

--- onebite-section02/src/pages/api/hello.ts
+++ onebite-section02/src/pages/api/hello.ts
@@ -1,13 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
   name: string;
 };
 
+// /api/hello 주소로 요청이 들어오면 실행되는 함수
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({ name: 'John Doe' });
 }
