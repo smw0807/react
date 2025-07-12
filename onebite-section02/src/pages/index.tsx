71501e07caa75be49a1be283a87c34a9d247a67e
smw0807 <smw0807@gmail.com>
2025-07-12 21:57:05 +0900

    SSG

--- onebite-section02/src/pages/index.tsx
+++ onebite-section02/src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { InferGetServerSidePropsType } from 'next';
+// import { InferGetServerSidePropsType } from 'next';
+import { InferGetStaticPropsType } from 'next';
 
 import SearchableLayout from '@/components/searchable-layout';
 import styles from './index.module.css';
@@ -8,10 +9,47 @@ import fetchRandomBooks from '@/lib/fetch-random-books';
 
 import BookItem from '@/components/book-item';
 
-// 서버 사이드 렌더링
-export const getServerSideProps = async () => {
-  // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
+// // 서버 사이드 렌더링
+// export const getServerSideProps = async () => {
+//   // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
+//   const [allBooks, randomBooks] = await Promise.all([
+//     fetchBooks(),
+//     fetchRandomBooks(),
+//   ]);
+//   return {
+//     props: {
+//       allBooks,
+//       randomBooks,
+//     },
+//   };
+// };
 
+// export default function Home({
+//   allBooks,
+//   randomBooks,
+// }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+//   return (
+//     <div className={styles.container}>
+//       <section>
+//         <h3>지금 추천하는 도서</h3>
+//         {randomBooks.map((book) => (
+//           <BookItem key={book.id} {...book} />
+//         ))}
+//       </section>
+//       <section>
+//         <h3>등록된 모든 도서</h3>
+//         {allBooks.map((book) => (
+//           <BookItem key={book.id} {...book} />
+//         ))}
+//       </section>
+//     </div>
+//   );
+// }
+
+// SSG
+export const getStaticProps = async () => {
+  // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
+  console.log('getStaticProps');
   const [allBooks, randomBooks] = await Promise.all([
     fetchBooks(),
     fetchRandomBooks(),
@@ -27,7 +65,7 @@ export const getServerSideProps = async () => {
 export default function Home({
   allBooks,
   randomBooks,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className={styles.container}>
       <section>
--- onebite-section02/src/pages/search/index.tsx
+++ onebite-section02/src/pages/search/index.tsx
@@ -1,23 +1,27 @@
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+// import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
 import SearchableLayout from '@/components/searchable-layout';
 
 import fetchBooks from '@/lib/fetch-books';
 import BookItem from '@/components/book-item';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { BookData } from '@/types';
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  const q = context.query.q;
-  const books = await fetchBooks(q as string);
-  return {
-    props: {
-      books,
-    },
+export default function Page() {
+  const [books, setBooks] = useState<BookData[]>([]);
+  const router = useRouter();
+  const q = router.query.q;
+
+  const fetchSearchResult = async () => {
+    const books = await fetchBooks(q as string);
+    setBooks(books);
   };
-};
-export default function Page({
-  books,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+
+  useEffect(() => {
+    if (q) {
+      fetchSearchResult();
+    }
+  }, [q]);
   return (
     <div>
       {books.map((book) => (
@@ -26,6 +30,28 @@ export default function Page({
     </div>
   );
 }
+// export const getServerSideProps = async (
+//   context: GetServerSidePropsContext
+// ) => {
+//   const q = context.query.q;
+//   const books = await fetchBooks(q as string);
+//   return {
+//     props: {
+//       books,
+//     },
+//   };
+// };
+// export default function Page({
+//   books,
+// }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+//   return (
+//     <div>
+//       {books.map((book) => (
+//         <BookItem key={book.id} {...book} />
+//       ))}
+//     </div>
+//   );
+// }
 
 Page.getLayout = (page: React.ReactNode) => {
   return <SearchableLayout>{page}</SearchableLayout>;
