f291a47f4118173d190346c6706fcf842077f15f
smw0807 <smw0807@gmail.com>
2025-07-20 21:52:02 +0900

    revalidatePath

--- onebite-section03/src/actions/create-review.action.ts
+++ onebite-section03/src/actions/create-review.action.ts
@@ -1,5 +1,7 @@
 'use server';
 
+import { revalidatePath } from 'next/cache';
+
 export async function createReviewAction(formData: FormData) {
   const bookId = formData.get('bookId')?.toString();
   const content = formData.get('content')?.toString();
@@ -22,6 +24,10 @@ export async function createReviewAction(formData: FormData) {
       }
     );
     console.log(response.status);
+    // next 서버에게 해당 경로를 다시 생성할 것을 요청함(재검증)
+    // 오직 서버측에서만 호출할 수 있음
+    // 모든 캐시를 무효화함, 풀 라우트 캐시도 삭제함
+    revalidatePath(`/book/${bookId}`);
   } catch (err) {
     console.error(err);
     return;
