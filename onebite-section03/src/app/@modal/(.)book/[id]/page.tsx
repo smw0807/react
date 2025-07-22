ab70f2c878e5f32af611f6dd46b761dadbe96277
smw0807 <smw0807@gmail.com>
2025-07-22 21:49:02 +0900

    패럴랠 & 인터셉팅 라우트

--- onebite-section03/src/app/@modal/(.)book/[id]/page.tsx
+++ onebite-section03/src/app/@modal/(.)book/[id]/page.tsx
@@ -2,11 +2,8 @@ import BookPage from '@/app/book/[id]/page';
 import Modal from '@/components/modal';
 export default function Page(props: any) {
   return (
-    <div>
-      가로채기!!
-      <Modal>
-        <BookPage {...props} />
-      </Modal>
-    </div>
+    <Modal>
+      <BookPage {...props} />
+    </Modal>
   );
 }
