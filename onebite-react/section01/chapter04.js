edbd3988f77b0c807fc4592f11b64d57ef1a7acd
smw0807 <smw0807@gmail.com>
2025-07-27 20:15:19 +0900

    자바스크립트 기본1

---
+++ onebite-react/section01/chapter04.js
@@ -0,0 +1,20 @@
+let age = 27;
+console.log(age);
+age = 34;
+console.log(age);
+
+const birth = '1992.08.07';
+console.log(birth);
+
+/**
+ * 변수 명명규칙(네이밍 규칙)
+ * $, _ 제외한 기호는 사용할 수 없다.
+ * 숫자로 시작할 수 없다.
+ * 예약어를 사용할 수 없다.(if, for, while, function, class, const, let, var, return, break, continue, try, catch, throw, etc)
+ *
+ * 변수 명명가이드
+ * 카멜케이스(camelCase) : 첫 글자는 소문자로 시작하고 두 번째 단어부터 대문자로 시작한다.
+ * 파스칼케이스(PascalCase) : 첫 글자는 대문자로 시작한다.
+ * 스네이크케이스(snake_case) : 단어와 단어 사이에 _를 사용한다.
+ * 케밥케이스(kebab-case) : 단어와 단어 사이에 -를 사용한다.
+ */
