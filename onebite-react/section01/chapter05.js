a57438510d21a7b2857a39a933d5c7c176fb436d
smw0807 <smw0807@gmail.com>
2025-07-27 20:51:11 +0900

    자바스크립트 기본2

--- onebite-react/section01/chapter05.js
+++ onebite-react/section01/chapter05.js
@@ -1,1 +1,12 @@
 console.log(undefined === null);
+
+const a = 1;
+const b = `${a}`;
+console.log(a, typeof a);
+console.log(b, typeof b);
+
+const str = '10개';
+const sttToNum = parseInt(str);
+const strToNum2 = Number(str);
+console.log(sttToNum, typeof sttToNum); //10 number
+console.log(strToNum2, typeof strToNum2); // NaN number
