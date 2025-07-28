a5ec599970ab51c4a5f9bfdd91cd571e93289b22
smw0807 <smw0807@gmail.com>
2025-07-28 20:50:27 +0900

    자바스크립트 기본5

---
+++ onebite-react/section01/chapter11.js
@@ -0,0 +1,17 @@
+// 함수 선언
+function greeting() {
+  console.log('안녕하세요!!');
+}
+
+greeting();
+
+function getArea(w, h) {
+  let area = w * h;
+  console.log(area);
+  return area;
+}
+getArea(10, 20);
+getArea(100, 200);
+
+// 호이스팅
+// -> 끌어올리다 라는 뜻
