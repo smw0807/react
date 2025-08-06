949c3e2e380abbecee6065c52bfd73daf36d9ac6
smw0807 <smw0807@gmail.com>
2025-08-06 12:29:26 +0900

    todo create

--- onebite-react/section08/src/App.tsx
+++ onebite-react/section08/src/App.tsx
@@ -1,13 +1,47 @@
 import './App.css';
+import { useState, useRef } from 'react';
 import Header from './components/Header';
 import Editor from './components/Editor';
 import List from './components/List';
 
+interface Todo {
+  id: number;
+  isDone: boolean;
+  content: string;
+  date: number;
+}
+
+const mockData: Todo[] = [
+  {
+    id: 0,
+    isDone: false,
+    content: 'React Study',
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: 'React Study2',
+    date: new Date().getTime(),
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(2);
+  const onCreate = (content: string) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content,
+      date: new Date().getTime(),
+    };
+    setTodos([...todos, newTodo]);
+  };
   return (
     <div className="App">
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   );
--- onebite-react/section08/src/components/Editor.tsx
+++ onebite-react/section08/src/components/Editor.tsx
@@ -1,10 +1,40 @@
+import { useState, useRef } from 'react';
 import './Editor.css';
 
-export default function Editor() {
+interface EditorProps {
+  onCreate: (content: string) => void;
+}
+
+export default function Editor({ onCreate }: EditorProps) {
+  const [content, setContent] = useState('');
+  const contentRef = useRef<HTMLInputElement>(null);
+  const onChangeContent = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setContent(e.target.value);
+  };
+  const onSubmit = () => {
+    if (content.length === 0) {
+      contentRef.current?.focus();
+      return;
+    }
+    onCreate(content);
+    setContent('');
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      onSubmit();
+    }
+  };
   return (
     <div className="Editor">
-      <input placeholder="새로운 Todo..." />
-      <button>추가</button>
+      <input
+        placeholder="새로운 Todo..."
+        value={content}
+        onChange={onChangeContent}
+        ref={contentRef}
+        onKeyDown={onKeyDown}
+      />
+      <button onClick={onSubmit}>추가</button>
     </div>
   );
 }
