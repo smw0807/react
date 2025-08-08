2b816c5d5f9b76946c1124b35ccdc25fce229d63
smw0807 <smw0807@gmail.com>
2025-08-08 12:33:14 +0900

    react-router-dom

--- onebite-react/section12/src/App.tsx
+++ onebite-react/section12/src/App.tsx
@@ -1,7 +1,21 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Diary from './pages/Diary';
+import New from './pages/New';
+import NotFound from './pages/NotFount';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/diary" element={<Diary />} />
+        <Route path="/new" element={<New />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
