bd3c86607ae3e5d17a63bd59069be499ec0ab15a
smw0807 <smw0807@gmail.com>
2025-08-12 12:27:04 +0900

    token api

--- vite-auth/src/api/auth.ts
+++ vite-auth/src/api/auth.ts
@@ -7,3 +7,25 @@ export async function login(username: string, password: string) {
     },
   });
 }
+
+// 토큰 검증
+export async function verifyToken(token: string) {
+  return fetch('http://localhost:5001/api/auth/verify/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+}
+
+// 토큰 재발급
+export async function refreshToken(token: string) {
+  return fetch('http://localhost:5001/api/auth/refresh/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+}
