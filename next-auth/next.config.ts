a2749726709e870485ba2c42a19e271df4af762b
smw0807 <smw0807@gmail.com>
2025-08-13 14:06:52 +0900

    프록시 설정 및 api

--- next-auth/next.config.ts
+++ next-auth/next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // 프록시 설정
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_API_URL}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
---
+++ next-auth/src/apis/auth.ts
@@ -0,0 +1,33 @@
+const API_BASE_URL = process.env.NEXT_API_URL || 'http://localhost:5001/api';
+
+export async function login(username: string, password: string) {
+  return fetch(`${API_BASE_URL}/auth/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Basic ${btoa(`${username}:${password}`)}`,
+    },
+  });
+}
+
+// 토큰 검증
+export async function verifyToken(token: string) {
+  return fetch(`${API_BASE_URL}/auth/verify/token`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: token,
+    },
+  });
+}
+
+// 토큰 재발급
+export async function refreshToken(token: string) {
+  return fetch(`${API_BASE_URL}/auth/refresh/token`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: token,
+    },
+  });
+}
