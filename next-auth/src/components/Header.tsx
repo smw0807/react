8466187704a6f36b4336a3a6bac66de8e0f6b85b
smw0807 <smw0807@gmail.com>
2025-08-14 13:47:39 +0900

    헤더 렌더링조건

--- next-auth/src/components/Header.tsx
+++ next-auth/src/components/Header.tsx
@@ -1,6 +1,14 @@
+'use client';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 function Header() {
+  const pathname = usePathname();
+
+  if (pathname === '/login') {
+    return null;
+  }
+
   return (
     <div className="flex justify-between items-center p-4">
       <div className="flex items-center gap-4">
