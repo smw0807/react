264c05661534569fef5520a440587ce08d733026
smw0807 <smw0807@gmail.com>
2025-11-20 22:30:18 +0900

    zustand middleware

--- onebite-exam/src/store/count.ts
+++ onebite-exam/src/store/count.ts
@@ -1,32 +1,65 @@
 import { create } from "zustand";
-import { combine } from "zustand/middleware";
+import {
+  combine,
+  subscribeWithSelector,
+  persist,
+  createJSONStorage,
+  devtools,
+} from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
 export const useCountStore = create(
-  immer(
-    combine(
-      // 입력된 state를 기준으로 자동으로 타입을 추론해줌.
+  devtools(
+    persist(
+      // 구독 기능
+      subscribeWithSelector(
+        // 불변성 유지
+        immer(
+          // 입력된 state를 기준으로 자동으로 타입을 추론해줌.
+          combine(
+            {
+              count: 0,
+            },
+            (set, get) => ({
+              actions: {
+                increase: () => {
+                  // set((state) => ({ count: state.count + 1 }));
+                  set((state) => {
+                    state.count += 1;
+                  });
+                },
+                decrease: () => {
+                  // set((state) => ({ count: state.count > 0 ? state.count - 1 : 0 }));
+                  set((state) => {
+                    state.count = state.count > 0 ? state.count - 1 : 0;
+                  });
+                },
+              },
+            }),
+          ),
+        ),
+      ),
       {
-        count: 0,
+        name: "count",
+        partialize: (state) => ({ count: state.count }),
+        storage: createJSONStorage(() => sessionStorage),
       },
-      (set, get) => ({
-        actions: {
-          increase: () => {
-            // set((state) => ({ count: state.count + 1 }));
-            set((state) => {
-              state.count += 1;
-            });
-          },
-          decrease: () => {
-            // set((state) => ({ count: state.count > 0 ? state.count - 1 : 0 }));
-            set((state) => {
-              state.count = state.count > 0 ? state.count - 1 : 0;
-            });
-          },
-        },
-      }),
     ),
+    {
+      name: "count",
+    },
   ),
+);
+
+useCountStore.subscribe(
+  (state) => state.count,
+  (count, prevCount) => {
+    console.log("count changed : ", count, "prevCount : ", prevCount);
+    // 현재 스토어 상태 가져오기
+    const store = useCountStore.getState();
+    // 스토어 상태 업데이트
+    // useCountStore.setState({ count: store.count + 1 });
+  },
 );
 // type Store = {
 //   count: number;
