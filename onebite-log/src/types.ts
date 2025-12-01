3a9fa685541b423c2c11feea7abfd5db55ae7485
smw0807 <smw0807@gmail.com>
2025-12-01 23:10:09 +0900

    supabase 셋팅

---
+++ onebite-log/src/types.ts
@@ -0,0 +1,5 @@
+import { type Database } from "@/database.types";
+
+export type PostEntity = Database["public"]["Tables"]["post"]["Row"];
+export type PostInsert = Database["public"]["Tables"]["post"]["Insert"];
+export type PostUpdate = Database["public"]["Tables"]["post"]["Update"];
