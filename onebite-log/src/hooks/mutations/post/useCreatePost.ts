857686b83d1bca6b600d45bc8709cbf071324f31
smw0807 <smw0807@gmail.com>
2025-12-22 22:53:09 +0900

    포스드 생성 수정 삭제 캐싱 처리

--- onebite-log/src/hooks/mutations/post/useCreatePost.ts
+++ onebite-log/src/hooks/mutations/post/useCreatePost.ts
@@ -1,12 +1,19 @@
 import { createPostWithImages } from "@/api/post";
+import { QUERY_KEYS } from "@/lib/constants";
 import type { UseMutationCallback } from "@/types";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useCreatePost(callbacks?: UseMutationCallback) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: createPostWithImages,
     onSuccess: () => {
       if (callbacks?.onSuccess) callbacks.onSuccess();
+      // 1. 캐시를 아예 초기화 해서 1페이지부터 다시 불러오게 하기
+      queryClient.resetQueries({ queryKey: QUERY_KEYS.post.list });
+      //// 2. 캐시 데이터에 완성된 포스트만 추가
+      //// 3. 낙관적 업데이트 방식
     },
     onError: (error) => {
       if (callbacks?.onError) callbacks.onError(error);
--- onebite-log/src/hooks/mutations/post/useDeletePost.ts
+++ onebite-log/src/hooks/mutations/post/useDeletePost.ts
@@ -1,9 +1,12 @@
 import { deleteImagesInPath } from "@/api/image";
 import { deletePost } from "@/api/post";
+import { QUERY_KEYS } from "@/lib/constants";
 import type { UseMutationCallback } from "@/types";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useDeletePost(callbacks?: UseMutationCallback) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: deletePost,
     onSuccess: async (data) => {
@@ -12,6 +15,10 @@ export function useDeletePost(callbacks?: UseMutationCallback) {
       if (data.image_urls && data.image_urls.length > 0) {
         await deleteImagesInPath(`${data.author_id}/${data.id}`);
       }
+      // 캐시 데이터 초기화
+      queryClient.resetQueries({
+        queryKey: QUERY_KEYS.post.list,
+      });
     },
     onError: (error) => {
       if (callbacks?.onError) callbacks.onError(error);
