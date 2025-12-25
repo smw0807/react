0645333e864159ca7314ad505044bb995b75f021
smw0807 <smw0807@gmail.com>
2025-12-25 18:33:37 +0900

    좋아요 결과 반영하기

--- onebite-log/src/hooks/mutations/post/useTogglePostLike.ts
+++ onebite-log/src/hooks/mutations/post/useTogglePostLike.ts
@@ -1,14 +1,43 @@
 import { togglePostLike } from "@/api/post";
-import type { UseMutationCallback } from "@/types";
-import { useMutation } from "@tanstack/react-query";
+import { QUERY_KEYS } from "@/lib/constants";
+import { type Post, type UseMutationCallback } from "@/types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useTogglePostLike(callbacks?: UseMutationCallback) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: togglePostLike,
+    onMutate: async ({ postId }) => {
+      await queryClient.cancelQueries({
+        queryKey: QUERY_KEYS.post.byId(postId),
+      });
+      // 이전 데이터 백업
+      const prevPost = queryClient.getQueryData<Post>(
+        QUERY_KEYS.post.byId(postId),
+      );
+      // 캐시 데이터 낙관적 업데이트
+      queryClient.setQueryData<Post>(QUERY_KEYS.post.byId(postId), (post) => {
+        if (!post) throw new Error("포스트가 존재하지 않습니다.");
+        return {
+          ...post,
+          isLiked: !post.isLiked,
+          like_count: post.isLiked ? post.like_count - 1 : post.like_count + 1,
+        };
+      });
+      // 실패 시 데이터 복구
+      return { prevPost };
+    },
     onSuccess: () => {
       if (callbacks?.onSuccess) callbacks.onSuccess();
     },
-    onError: (error) => {
+    onError: (error, _, context) => {
+      if (context && context.prevPost) {
+        queryClient.setQueryData<Post>(
+          QUERY_KEYS.post.byId(context.prevPost.id),
+          context.prevPost,
+        );
+      }
       if (callbacks?.onError) callbacks.onError(error);
     },
   });
